df9fe2a5bd31f83a63ef625e073834869af462b5
Michael K Johnson <a1237@danlj.org>
2026-09-07 15:13:05 -0400

    Apply gofmt

--- waveloggoat.go
+++ waveloggoat.go
@@ -61,12 +61,12 @@ type RigData struct {
 
 // WavelogJSONRequest matches the required JSON payload for the Wavelog API update.
 type WavelogJSONRequest struct {
-	Radio       string   `json:"radio"`
+	Radio       string      `json:"radio"`
 	Power       interface{} `json:"power,omitempty"`
-	Frequency   int      `json:"frequency"`
-	Mode        string   `json:"mode"`
-	FrequencyRX int      `json:"frequency_rx,omitempty"`
-	ModeRX      string   `json:"mode_rx,omitempty"`
+	Frequency   int         `json:"frequency"`
+	Mode        string      `json:"mode"`
+	FrequencyRX int         `json:"frequency_rx,omitempty"`
+	ModeRX      string      `json:"mode_rx,omitempty"`
 }
 
 type WavelogErrorResponse struct {
